826d900dca898520878a4af78a8e0d6b84e568de
michaellskyes <Michael Li>
2015-11-09 16:46:00 -0500

    not working rube boat

---
+++ Physcis stuff/Assets/Scripts/boatMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boatMove : MonoBehaviour {
+
+	public bool move;
+
+	// Use this for initialization
+	void Start () {
+	
+		move = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		if(move)
+		{
+			transform.position +=transform.forward;
+		}
+	}
+}
---
+++ Physcis stuff/Assets/Scripts/boatTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boatTrigger : MonoBehaviour {
+
+	public boatMove myboat;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		myboat.move = true;
+	}
+}
